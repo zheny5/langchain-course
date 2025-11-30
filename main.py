ef4e14078568bc9fea274b2b8a2bd7688f4451bc
zheny5 <17645002906@163.com>
2025-11-30 15:56:09 +0800

    Use DeepSeek chat model

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
 from langchain_ollama import ChatOllama
+from langchain_deepseek import ChatDeepSeek
 
 load_dotenv()
 
@@ -31,7 +32,8 @@ Musk's political activities, views, and statements have made him a polarizing fi
     )
 
     # llm = ChatOllama(temperature=0, model="gemma3:270m")
-    llm = ChatOpenAI(temperature=0, model="gpt-5")
+    # llm = ChatOpenAI(temperature=0, model="gpt-5")
+    llm = ChatDeepSeek(temperature=0, model="deepseek-chat")
     chain = summary_prompt_template | llm
 
     response = chain.invoke(input={"information": information})
