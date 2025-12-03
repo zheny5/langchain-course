b89cb6326b89f507857869e46ac1079ce7c1ce17
zheny5 <17645002906@163.com>
2025-12-03 22:55:31 +0800

    Update rag gist sample

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ from langchain_classic.chains.combine_documents import create_stuff_documents_ch
 from langchain_classic.chains.retrieval import create_retrieval_chain
 from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_deepseek import ChatDeepSeek
 from langchain_pinecone import PineconeVectorStore
+from langchain_huggingface import HuggingFaceEmbeddings
+from pinecone import Pinecone
 
 load_dotenv()
 
@@ -14,16 +17,36 @@ load_dotenv()
 if __name__ == "__main__":
     print(" Retrieving...")
 
-    embeddings = OpenAIEmbeddings()
-    llm = ChatOpenAI()
+    # embeddings = OpenAIEmbeddings()
+    embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+
+    llm = ChatDeepSeek(model="deepseek-chat")
 
     query = "what is Pinecone in machine learning?"
     chain = PromptTemplate.from_template(template=query) | llm
+    api_key = os.environ["PINECONE_API_KEY"]
+    pc = Pinecone(api_key=api_key)
     # result = chain.invoke(input={})
     # print(result.content)
 
+    # Create index with integrated embedding model
+    index_name = "my-index"
+    if not pc.has_index(index_name):
+        pc.create_index_for_model(
+            name=index_name,
+            cloud="aws",  # or "gcp", "azure"
+            region="us-east-1",  # choose based on location
+            embed={
+                "model": "llama-text-embed-v2",  # recommended for most cases
+                "field_map": {"text": "content"}  # maps search field to record field
+            }
+        )
+
+    index = pc.Index(index_name)
+
+
     vectorstore = PineconeVectorStore(
-        index_name=os.environ["INDEX_NAME"], embedding=embeddings
+        index_name=index_name, embedding=embeddings
     )
 
     retrieval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
